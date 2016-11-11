57bea86eb514d917b502e0e83a8b135e8ac5cd4a
Enrico Lumetti <enrico.lumetti@gmail.com>
2016-11-11 14:56:13 +0100

    Week 2

---
+++ corsopython/easydraw.py
@@ -0,0 +1,46 @@
+import tkinter
+
+__all__ = ["create_window", "draw_line", "draw_rectangle", "draw_pixel", "clear"]
+
+_cv = None
+
+
+def create_window(width, height):
+    """
+    Create a window with specified dimensions.
+    Must be called at the beginning, only one time.
+    """
+    global _cv
+
+    if _cv is not None:
+        return
+    
+    master = tkinter.Tk()
+    _cv = tkinter.Canvas(master, width=width, height=height)
+    _cv.pack()
+
+def draw_line(x, y, w, z, color):
+    """
+    Draw a line from point (x,y) to point (w,z) of the specified color
+    """
+    _cv.create_line(x, y, w, z, fill=color)
+
+def draw_rectangle(x, y, width, height, color):
+    """
+    Draw a rectangle with top-left corner in (x,y), with specified width, height (in pixels) and color
+    """
+    _cv.create_rectangle(x, y, x+width, y+height, state=tkinter.DISABLED, outline='', fill=color)
+
+def draw_pixel(x, y, color):
+    """ Set pixel (x, y) to color"""
+    draw_rectangle(x, y, 1, 1, color)
+
+def clear():
+    """
+    Clears the screen
+    """
+    _cv.delete("all")
+    
+
+
+#mainloop()
